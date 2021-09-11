3e7f8f4bf92be8838ae7798d80c0a7e84f65d52d
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-09-11 01:07:10 -0400

    Created base of SitePresentatio component

---
+++ src/components/SitePresentation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="site-presentation">
+    <div class="site-presentation__header-container">
+      <h1 class="site-presentation__header">
+        Welcome To Tech Quiz!!
+        <br />
+        <span class="site-presentation__secondary-info">
+          Take a quiz and test your knowledge about any tech topic!!!
+        </span>
+      </h1>
+    </div>
+  </div>
+</template>
+// :src="require(`../../assets/imgs/${avatar.filename}`)"
+<script>
+import { reactive, toRefs } from "@vue/reactivity";
+export default {
+  name: "SitePresentation",
+  setup() {
+    const data = reactive({});
+    return { ...toRefs(data) };
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <site-presentation></site-presentation>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import SitePresentation from "@/components/SitePresentation";
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: { SitePresentation },
 };
 </script>

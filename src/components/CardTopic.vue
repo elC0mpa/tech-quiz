490e692b9f8f9bf2c12878196cbd084b7179fd78
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-09-12 23:21:26 -0400

    Added base og quizzes-selection layout

---
+++ src/components/CardTopic.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-topic">
+    <img
+      class="card-topic__svg"
+      :src="require(`../assets/svgs/${quiz.topic.toLowerCase()}.svg`)"
+    />
+    <p class="card-topic__text">{{ quiz.topic }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardTopic",
+  props: {
+    quiz: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup(props) {
+    return { props };
+  },
+};
+</script>
--- src/components/QuizzesSelection.vue
+++ src/components/QuizzesSelection.vue
@@ -1,12 +1,69 @@
 <template>
-  <div class="quizzes-selection"></div>
+  <div class="quizzes-selection">
+    <p class="quizzes-selection__main-text">Select a topic</p>
+    <div class="quizzes-selection__container">
+      <card-topic
+        v-for="(quiz, index) in quizzes"
+        :key="index"
+        :quiz="quiz"
+      ></card-topic>
+    </div>
+  </div>
 </template>
 
 <script>
+import { reactive, toRefs } from "@vue/reactivity";
+import CardTopic from "./CardTopic.vue";
 export default {
   name: "QuizzesSelection",
+  components: {
+    CardTopic,
+  },
   setup() {
-    return {};
+    const data = reactive({
+      quizzes: [
+        {
+          topic: "Linux",
+        },
+        // {
+        //   topic: "DevOps",
+        // },
+        {
+          topic: "Python",
+        },
+        {
+          topic: "JavaScript",
+        },
+        // {
+        //   topic: "Networking",
+        // },
+        {
+          topic: "PHP",
+        },
+        // {
+        //   topic: "Bash",
+        // },
+        {
+          topic: "HTML5",
+        },
+        {
+          topic: "Docker",
+        },
+        {
+          topic: "Kubernetes",
+        },
+        {
+          topic: "WordPress",
+        },
+        {
+          topic: "Laravel",
+        },
+        {
+          topic: "Cloud",
+        },
+      ],
+    });
+    return { ...toRefs(data) };
   },
 };
 </script>

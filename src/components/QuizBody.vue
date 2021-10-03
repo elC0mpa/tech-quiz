ee533b178d3c0552ba4a497a150bb307f6a36af4
Jose Gabriel Companioni Benitez <gcompanionib@gmail.com>
2021-10-03 18:33:26 -0400

    Added placeholders in QuizBody component

--- src/components/QuizBody.vue
+++ src/components/QuizBody.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="quiz-body" v-if="actualQuestion">
-    <p class="quiz-body__question">
+  <div class="quiz-body">
+    <p v-if="!isLoading" class="quiz-body__question">
       {{ actualQuestion.question }}
     </p>
-    <answer-item
-      v-for="(answer, index) in answers"
-      :key="index"
-      :answer="answer"
-      :number="index + 1"
-      @click="answerSelected(answer)"
-    ></answer-item>
+    <content-loader v-else viewBox="0 0 100 3" secondaryColor="#4756ca">
+      <rect x="0" y="0" width="100%" height="100%" />
+    </content-loader>
+    <div v-if="!isLoading">
+      <answer-item
+        v-for="(answer, index) in answers"
+        :key="index"
+        :answer="answer"
+        :number="index + 1"
+        @click="answerSelected(answer)"
+      ></answer-item>
+    </div>
+    <div v-else class="quiz-body__placeholders-container">
+      <content-loader viewBox="0 0 100 3" secondaryColor="#3186b2">
+        <rect x="0" y="0" width="100%" height="100%" />
+      </content-loader>
+      <content-loader viewBox="0 0 100 3" secondaryColor="#3186b2">
+        <rect x="0" y="0" width="100%" height="100%" />
+      </content-loader>
+      <content-loader viewBox="0 0 100 3" secondaryColor="#3186b2">
+        <rect x="0" y="0" width="100%" height="100%" />
+      </content-loader>
+    </div>
   </div>
 </template>
 
@@ -17,14 +33,17 @@
 import { computed } from "@vue/reactivity";
 import { useStore } from "vuex";
 import AnswerItem from "./AnswerItem.vue";
+import { ContentLoader } from "vue-content-loader";
 export default {
   name: "QuizBody",
   components: {
     AnswerItem,
+    ContentLoader,
   },
   setup() {
     const store = useStore();
     const actualQuestion = computed(() => store.getters.actualQuestion);
+    const isLoading = computed(() => store.getters.isLoadingQuestions);
     const answers = computed(() => {
       const answers = [];
       Object.keys(actualQuestion.value.answers).forEach((label) => {
@@ -35,12 +54,12 @@ export default {
           });
         }
       });
-      return answers;
+      return isLoading.value ? [] : answers;
     });
     const answerSelected = (answer) => {
       store.commit("setSelectedAnswer", answer);
     };
-    return { actualQuestion, answers, answerSelected };
+    return { actualQuestion, answers, answerSelected, isLoading };
   },
 };
 </script>
@@ -52,6 +71,11 @@ export default {
     font-size: x-large;
     font-weight: bold;
     color: $question-color;
+  }
+  &__placeholders-container {
+    > * {
+      margin-top: $gap;
+    }
   }
 }
 </style>
